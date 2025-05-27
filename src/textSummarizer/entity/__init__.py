fe3fa80adac95721742077589b63863afd34ea63
stevenhan777 <120138752+stevenhan777@users.noreply.github.com>
2025-05-27 09:25:20 -0400

    data transformation completed

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -18,11 +18,11 @@ class DataValidationConfig:
 
 
 
-# @dataclass(frozen=True)
-# class DataTransformationConfig:
-#     root_dir: Path
-#     data_path: Path
-#     tokenizer_name: Path
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
 
 
 
