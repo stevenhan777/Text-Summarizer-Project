dedb08ba250e48c54f47ec6b7222f9affbfb2ebe
stevenhan777 <120138752+stevenhan777@users.noreply.github.com>
2025-05-28 08:40:03 -0400

    model trainer completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -2,8 +2,8 @@ from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 from textSummarizer.entity import (DataIngestionConfig,
                                    DataValidationConfig,
-                                   DataTransformationConfig)
-#                                    ModelTrainerConfig,
+                                   DataTransformationConfig,
+                                   ModelTrainerConfig)
 #                                    ModelEvaluationConfig)
 
 
@@ -65,28 +65,28 @@ class ConfigurationManager:
     
 
 
-    # def get_model_trainer_config(self) -> ModelTrainerConfig:
-    #     config = self.config.model_trainer
-    #     params = self.params.TrainingArguments
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
 
-    #     create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
-    #     model_trainer_config = ModelTrainerConfig(
-    #         root_dir=config.root_dir,
-    #         data_path=config.data_path,
-    #         model_ckpt = config.model_ckpt,
-    #         num_train_epochs = params.num_train_epochs,
-    #         warmup_steps = params.warmup_steps,
-    #         per_device_train_batch_size = params.per_device_train_batch_size,
-    #         weight_decay = params.weight_decay,
-    #         logging_steps = params.logging_steps,
-    #         evaluation_strategy = params.evaluation_strategy,
-    #         eval_steps = params.evaluation_strategy,
-    #         save_steps = params.save_steps,
-    #         gradient_accumulation_steps = params.gradient_accumulation_steps
-    #     )
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt = config.model_ckpt,
+            num_train_epochs = params.num_train_epochs,
+            warmup_steps = params.warmup_steps,
+            per_device_train_batch_size = params.per_device_train_batch_size,
+            weight_decay = params.weight_decay,
+            logging_steps = params.logging_steps,
+            eval_strategy = params.eval_strategy, # changed from evaluation_strategy
+            eval_steps = params.eval_steps,
+            save_steps = params.save_steps,
+            gradient_accumulation_steps = params.gradient_accumulation_steps
+        )
 
-    #     return model_trainer_config
+        return model_trainer_config
     
 
     # def get_model_evaluation_config(self) -> ModelEvaluationConfig:
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -26,20 +26,20 @@ class DataTransformationConfig:
 
 
 
-# @dataclass(frozen=True)
-# class ModelTrainerConfig:
-#     root_dir: Path
-#     data_path: Path
-#     model_ckpt: Path
-#     num_train_epochs: int
-#     warmup_steps: int
-#     per_device_train_batch_size: int
-#     weight_decay: float
-#     logging_steps: int
-#     evaluation_strategy: str
-#     eval_steps: int
-#     save_steps: float
-#     gradient_accumulation_steps: int
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
+    num_train_epochs: int
+    warmup_steps: int
+    per_device_train_batch_size: int
+    weight_decay: float
+    logging_steps: int
+    eval_strategy: str # changed from evaluation_strategy
+    eval_steps: int
+    save_steps: float
+    gradient_accumulation_steps: int
 
 
 
